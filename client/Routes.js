29636ffe5ce63e71e0f871ea350be9ee51f5a99e
Karina Yang <karinakeen@icloud.com>
2017-08-11 16:28:27 -0400

    FEAT successfully render node and arrow

--- client/Routes.js
+++ client/Routes.js
@@ -4,7 +4,7 @@ import {Router} from 'react-router'
 import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome} from './components'
+import {Main, Login, Signup, UserHome, Node} from './components'
 import {me} from './store'
 
 /**
@@ -35,6 +35,7 @@ class Routes extends Component {
                 </Switch> : null
             }
             {/* Displays our Login component as a fallback */}
+            <Route component={Node} />
             <Route component={Login} />
           </Switch>
         </Main>
