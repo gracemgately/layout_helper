08cefc1e826eb4ce6c82c2bb6e576521fc02fb28
Karina Yang <karinakeen@icloud.com>
2017-08-17 11:53:10 -0400

    CLEAN console.log

--- client/components/DrawNode.js
+++ client/components/DrawNode.js
@@ -59,12 +59,8 @@ export const breadthFirstForEach = (node) => {
     //console.log("LEVEL", level);
     collection.push([drawBSTNode2(tree), level]);
 
-    if (tree.left) {
-      queue.push(tree.left)
-    };
-    if (tree.right) {
-      queue.push(tree.right)
-    };
+    if (tree.left) queue.push(tree.left)
+    if (tree.right) queue.push(tree.right)
   }
   return collection;
 }
