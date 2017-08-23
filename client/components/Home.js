0a0189b8a917989f2f418c936eb1a9d56d2bbddc
Julia Morley <juliamorley@users.noreply.github.com>
2017-08-23 12:57:30 -0400

    modernized homepage

--- client/components/Home.js
+++ client/components/Home.js
@@ -18,7 +18,7 @@ export default class Home extends Component {
                     <div className="card">
                         <Link to={`/linked-list`}>
                             <div className="col-xs-10">
-                                <img src="/images/ll.png" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%'}}></img>
+                                <img src="/images/singlell.jpg" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%'}}></img>
                                 <h4>Linked List</h4>
                             </div>
                         </Link>
@@ -27,7 +27,7 @@ export default class Home extends Component {
                     <div className="card">
                         <Link to={`/binary-search-tree`}>
                             <div className="col-xs-10">
-                                <img src="/images/bst.png" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%' }}></img>
+                                <img src="/images/bstblack.jpg" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%' }}></img>
                                 <h4>Binary Search Tree</h4>
                             </div>
                         </Link>
@@ -36,7 +36,7 @@ export default class Home extends Component {
                     <div className="card">
                         <Link to={`/queue`}>
                             <div className="col-xs-10">
-                                <img src="/images/queue.png" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%' }}></img>
+                                <img src="/images/queue.jpg" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%' }}></img>
                                 <h4>Queue</h4>
                             </div>
                         </Link>
@@ -45,7 +45,7 @@ export default class Home extends Component {
                     <div className="card">
                         <Link to={`/stack`}>
                             <div className="col-xs-10">
-                                <img src="/images/stack.png" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%'}}></img>
+                                <img src="/images/stack.jpg" alt="Avatar" style={{ width: 90 + '%', height: 100 + '%'}}></img>
                                 <h4>Stack</h4>
                             </div>
                         </Link>
