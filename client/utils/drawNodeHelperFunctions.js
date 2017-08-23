64ba874f08069470817953e3302c481ca1a2af23
Carol Dysart <dysart.carol@gmail.com>
2017-08-23 18:26:02 -0400

    changed color of nodes to orange

--- client/utils/drawNodeHelperFunctions.js
+++ client/utils/drawNodeHelperFunctions.js
@@ -9,13 +9,13 @@ export const drawBasicNode = (node, toggled, index, highlightIndex) => {
               <stop offset="95%" stopColor="yellow" />
             </linearGradient>
             <linearGradient id="MyGradient2">
-              <stop offset="5%" stopColor="white" />
-              <stop offset="95%" stopColor="#87ceeb" />
+              <stop offset="5%" stopColor="orange" />
+              <stop offset="95%" stopColor="#db6e08" />
             </linearGradient>
           </defs>
         <circle className={toggled === true && index === highlightIndex ? "yellow" : "none"} key={index} cx="25" cy="25" r="25"> </circle>
 
-        <text x="50%" y="50%" textAnchor="middle" stroke="#51c5cf " strokeWidth="2px" dy=".3em">{node.value}</text>
+        <text x="50%" y="50%" textAnchor="middle" stroke="black " strokeWidth="2px" dy=".3em">{node.value}</text>
       </svg>
   );
 }
