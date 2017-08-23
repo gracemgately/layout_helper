743575eee25f15e1353e77ad8c17aac813da4bdf
Carol Dysart <dysart.carol@gmail.com>
2017-08-23 17:09:44 -0400

    updated background color and height

--- client/components/SingleUserDS.js
+++ client/components/SingleUserDS.js
@@ -18,40 +18,6 @@ class SingleUserDS extends Component {
 
   componentDidMount() {
     store.dispatch(FetchUserDS(this.props.user.id));
-
-    var canvas = this.refs.myCanvas;
-    var c = canvas.getContext("2d");
-
-    function animate() {
-      //draw the container
-      c.fillStyle = "#EDEAE5";
-      c.fillRect(container.x, container.y, container.width, container.height);
-
-      //loop throughj the circles array
-      for (var i = 0; i < circles.length; i++) {
-        //draw the circles
-        c.fillStyle = 'hsl(' + circles[i].color++ + ', 57%, 47%)';
-        c.beginPath();
-        c.arc(circles[i].x, circles[i].y, circles[i].r, 0, Math.PI * 2, true);
-        c.fill()
-
-        //time to animate our circles ladies and gentlemen.
-        if (circles[i].x - circles[i].r + circles[i].vx < container.x || circles[i].x + circles[i].r + circles[i].vx > container.x + container.width) {
-          circles[i].vx = -circles[i].vx;
-        }
-
-        if (circles[i].y + circles[i].r + circles[i].vy > container.y + container.height || circles[i].y - circles[i].r + circles[i].vy < container.y) {
-          circles[i].vy = -circles[i].vy;
-        }
-
-        circles[i].x += circles[i].vx
-        circles[i].y += circles[i].vy
-      }
-
-      requestAnimationFrame(animate);
-    }
-    requestAnimationFrame(animate);
-
   }
 
   render() {
@@ -120,7 +86,6 @@ class SingleUserDS extends Component {
             </div>
           </div>
         </div>
-        <canvas ref="myCanvas" className="bounce-canvas"></canvas>
       </div>
     )
   }
@@ -144,65 +109,3 @@ const mapState = (state) => {
 export default connect(mapState, null)(SingleUserDS);
 
 
-//ANIMATION FUNCTIONALITY BY GICHUHI MBUGUA, 2015: 
-//https://codepen.io/gichmbugua/pen/LpZWgo
-
-
-//create the container that will hold the boincing balls.
-    var container = {
-      x: 0,
-      y: 0,
-      width: 400,
-      height: 200
-    };
-//create the array of circles that will be animated
-    var circles = [{
-      x: 50,
-      y: 100,
-      r: 5,
-      vx: 1,
-      vy: 1,
-      color: 173
-    }, {
-      x: 150,
-      y: 80,
-      r: 10,
-      vx: 1,
-      vy: 1,
-      color: 173
-    }, {
-      x: 90,
-      y: 150,
-      r: 5,
-      vx: 1,
-      vy: 1,
-      color: 173
-    }, {
-      x: 100,
-      y: 50,
-      r: 2,
-      vx: 1,
-      vy: 1,
-      color: 173
-    }, {
-      x: 100,
-      y: 50,
-      r: 2,
-      vx: 1,
-      vy: 1,
-      color: 173
-    },{
-      x: 100,
-      y: 50,
-      r: 2,
-      vx: 1,
-      vy: 1,
-      color: 173
-    }, {
-      x: 100,
-      y: 50,
-      r: 2,
-      vx: 1,
-      vy: 1,
-      color: 173
-    }];
